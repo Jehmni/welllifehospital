a50d06f74531b2685ccc8e70e9c26adfd74bb9e9
Jehmni <jehmnitech@gmail.com>
2026-01-02 17:19:33 +0000

    Update walkthrough and documentation

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,16 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
   },
+  server: {
+    port: 8080,
+  }
 });
