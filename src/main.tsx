8d6e1a41fdb6f3ed234d541a3bb12bfb935231db
Jehmni <jehmnitech@gmail.com>
2026-01-02 16:50:52 +0000

    Standardize entry points and fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './index.css'
-
-const queryClient = new QueryClient()
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<App />} />
-                    {/* Add Portal routes here as needed */}
-                </Routes>
-            </BrowserRouter>
-        </QueryClientProvider>
+        <App />
     </React.StrictMode>
-)
+);
